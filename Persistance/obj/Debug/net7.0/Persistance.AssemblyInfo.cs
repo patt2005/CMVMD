fe4030f323477bc2a0a8ff21af69862ea9508fa2
Peter Grigor <petrupewdiepie@gmail.com>
2024-08-20 11:40:31 +0300

    visual changes

--- Persistance/obj/Debug/net7.0/Persistance.AssemblyInfo.cs
+++ Persistance/obj/Debug/net7.0/Persistance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9973ade26e8a9d3efe5597f1e793f2d0f2e0c35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204696204b56eddf57b773eeb744a73c4bc83d16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
