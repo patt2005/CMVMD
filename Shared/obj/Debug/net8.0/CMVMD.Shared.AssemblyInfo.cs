817da666d55b91bcd900970d7cf1148becd16471
Peter Grigor <petrupewdiepie@gmail.com>
2024-09-07 14:57:24 +0300

    event fix commit

--- Shared/obj/Debug/net8.0/CMVMD.Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/CMVMD.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMVMD.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304da54f161e42d539629e1c45c043d1a23db9e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf24acdb17050040241e49c5d2b593a589dde331")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMVMD.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMVMD.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
